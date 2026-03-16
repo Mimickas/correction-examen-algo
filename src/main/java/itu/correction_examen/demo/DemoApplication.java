958237cfd8e03605492ff3926e5f2c86447278b9
Toky <toky.andrianina401@gmail.com>
2026-03-16 08:16:23 +0300

    [projet maven][init] initialisation

---
+++ src/main/java/itu/correction_examen/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package itu.correction_examen.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
